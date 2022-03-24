a25c57a0676eea6904f607ca3c6921069a84b48d
Aman Yadav <84045140+amylucky2002@users.noreply.github.com>
2022-03-24 20:05:20 +0530

    first commit

---
+++ C Progam/first.cpp
@@ -0,0 +1,41 @@
+/* #include<iostream>
+using namespace std;
+int main()
+{
+    cout<< "Hello World ! its AMAN DATE 23" <<endl;
+    return 0;
+}
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<< "/|"<< endl;
+    cout<<"/ |"<< endl;
+   cout<<"/  |"<< endl;
+  cout<<"/   |"<< endl;
+cout<< "/____|"<< endl;
+return 0;
+}   
+*/
+
+// Variable 
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    string charactorname =" Mahak ";
+    int charactorage;
+    charactorage =  18  ;
+
+    cout<< "There are the palace where " << charactorname << "lived in Gughli"<< endl;
+    cout<< "She was " << charactorage << "old on that time "<< endl;  
+    charactorname = " Aman";
+    cout<< "she did not like " << charactorname<< endl;
+    cout<<" She did like her age is " << charactorage << endl;
+    return 0;
+    }
+  
